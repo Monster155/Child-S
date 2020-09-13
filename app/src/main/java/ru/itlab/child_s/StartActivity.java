39d5695a01fe536ac5108321a191aaab41d4afc9
Damir D <damir180501@mail.ru>
2020-09-13 14:54:10 +0300

    Added Start Screen (without design)

---
+++ app/src/main/java/ru/itlab/child_s/StartActivity.java
@@ -0,0 +1,54 @@
+package ru.itlab.child_s;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Pair;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class StartActivity extends Activity {
+
+    EditText text, password;
+    TextView error;
+    Button login;
+    ImageView logo;
+    ArrayList<Pair<String, String>> logpass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        logpass = new ArrayList<>();
+        logpass.add(new Pair("koala", "0000"));
+        logpass.add(new Pair("dajjsand", "12345678"));
+        logpass.add(new Pair("korjick", "87654321"));
+
+        login = findViewById(R.id.button);
+        text = findViewById(R.id.editText);
+        password = findViewById(R.id.editTextPassword);
+        logo = findViewById(R.id.imageView);
+        error = findViewById(R.id.textView);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (logpass.indexOf(new Pair(
+                        text.getText().toString().toLowerCase(),
+                        password.getText().toString().toLowerCase())) != -1) {
+                    Intent i = new Intent(StartActivity.this, MainActivity.class);
+                    startActivity(i);
+                } else {
+                    error.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+    }
+}
